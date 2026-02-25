fa4a5e4aacb1c64195ddb101cb26940595d8439a
Zamantha Palionay <palionaynicole@gmail.com>
2026-02-25 18:07:11 +0800

    my-first-angular

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+  title = 'Essence Perfume Shop';
+}
